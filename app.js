08d8d666c7fac383476bb830193fa4174de91d29
Monirul <monirul.molla.cts@gmail.com>
2018-11-12 18:49:25 -0500

    updated the CRUD functionality
    updated the CRUD functionality

--- app.js
+++ app.js
@@ -1,120 +1,117 @@
 var express = require("express");
+var bodyParser = require('body-parser')
+
+var uuid = require('node-uuid');
 var app = express();
-let developers =  [
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+
+let developers = [
     {
-        "id": 1,
-        "firstName": "Jason",
-        "lastName": "Monroe",
-        "favoriteLanguage": "C#",
+        "id": "a57537a8-6290-4b8b-b0cb-e7de6b000b77",
+        "firstName": "Monirul",
+        "lastName": "Molla",
+        "favoriteLanguage": "Java",
         "yearStarted": 2008,
         "imagePath": null
     },
     {
-        "id": 2,
+        "id": "338f0585-6737-48a0-b4be-d88fc011b72d",
         "firstName": "Bill",
         "lastName": "Gates",
         "favoriteLanguage": "BASIC",
         "yearStarted": 1978,
         "imagePath": null
-    },
-    {
-        "id": 4,
-        "firstName": "Steve",
-        "lastName": "Wozniak",
-        "favoriteLanguage": "Pascal",
-        "yearStarted": 1977,
-        "imagePath": null
-    },
-    {
-        "id": 49,
-        "firstName": "Bon",
-        "lastName": "Jovi",
-        "favoriteLanguage": "C++",
-        "yearStarted": 1998,
-        "imagePath": null
-    },
-    {
-        "id": 50,
-        "firstName": "Bjarn",
-        "lastName": "Stroustrop",
-        "favoriteLanguage": "C++",
-        "yearStarted": 1980,
-        "imagePath": null
-    },
-    {
-        "id": 51,
-        "firstName": "sandya",
-        "lastName": "palaparthy",
-        "favoriteLanguage": "java",
-        "yearStarted": 2007,
-        "imagePath": null
-    },
-    {
-        "id": 52,
-        "firstName": "Michael",
-        "lastName": "Jackson",
-        "favoriteLanguage": "English",
-        "yearStarted": 1970,
-        "imagePath": null
-    },
-    {
-        "id": 53,
-        "firstName": "Krishna",
-        "lastName": "Manthena",
-        "favoriteLanguage": "Ionic",
-        "yearStarted": 2018,
-        "imagePath": null
-    },
-    {
-        "id": 54,
-        "firstName": "Krishna",
-        "lastName": "Manthena",
-        "favoriteLanguage": "Ruby On Rails",
-        "yearStarted": 2010,
-        "imagePath": null
-    },
-    {
-        "id": 55,
-        "firstName": "Manthena",
-        "lastName": "Krishna",
-        "favoriteLanguage": "ReactJs",
-        "yearStarted": 2013,
-        "imagePath": null
-    },
-    {
-        "id": 56,
-        "firstName": "Ramesh",
-        "lastName": "Selvarasu",
-        "favoriteLanguage": "PHP",
-        "yearStarted": 2016,
-        "imagePath": null
-    },
-    {
-        "id": 57,
-        "firstName": "Prasad",
-        "lastName": "Duggirala",
-        "favoriteLanguage": "Angular",
-        "yearStarted": 2014,
-        "imagePath": null
-    },
-    {
-        "id": 58,
-        "firstName": "Ragu",
-        "lastName": "Thirumoorthy",
-        "favoriteLanguage": "Angular",
-        "yearStarted": 2004,
-        "imagePath": null
     }
 ];
+
+//To fetch all bios
 app.get("/users", (req, res, next) => {
-  
     res.json(developers);
 });
 
-app.post("/addUser", (req, res, next) => {
-  
-    res.json(developers);
+//To save a bio
+app.post("/addBio", (req, res, next) => {
+    if(!req.body && !req.body.firstName) {
+        return res.send({"status": "error", "message": "missing username"});
+    } else {
+        let params = req.body
+        addBio(params);
+        console.log(JSON.stringify(params, Object.keys(params).sort()));
+        return res.send(developers);
+    }
 });
+
+//To fetch a bio with respect to id
+app.get('/bio/:id', (req, res) =>{
+    if(!req.params.id) {
+        return res.send({"status": "error", "message": "missing id"});
+    } else {
+        var devId = req.params.id;
+        let myBio = getBioById(devId);
+        return res.send(myBio); 
+    }
+});
+
+//To update a bio with respect to id
+
+app.put("/updateBio", (req, res, next) => {
+    if(!req.body && !req.body.firstName) {
+        return res.send({"status": "error", "message": "missing username"});
+    } else {
+        let params = req.body
+        updateBio(params);
+        console.log(JSON.stringify(params, Object.keys(params).sort()));
+        return res.send(developers);
+    }
+});
+
+//To delete a bio with respect to id
+app.delete('/deleteBio/:id', (req, res) =>{
+    if(!req.params.id) {
+        return res.send({"status": "error", "message": "missing id"});
+    } else {
+        var devId = req.params.id;
+        deleteBio(devId);
+        return res.send(developers); 
+    }
+});
+
+
+function addBio(params) {
+    var newBio = {
+        "id": uuidv4(),
+        "firstName": params.firstName,
+        "lastName": params.lastName,
+        "favoriteLanguage": params.favoriteLanguage,
+        "yearStarted": params.yearStarted,
+        "imagePath": null
+    }
+    developers.push(newBio);
+}
+
+function getBioById(devId){
+    let bio = developers.filter(dev => dev.id == devId);
+    return bio;
+}
+
+function deleteBio(devId){
+    var index = developers.findIndex(function(o){
+        return o.id === devId;
+    });
+    if (index !== -1) 
+        developers.splice(index, 1);
+}
+
+function updateBio(dev){
+    developers[developers.findIndex(el => el.id === dev.id)] = dev;
+}
+
+function uuidv4() {
+    return uuid.v4();
+}
+
 app.listen(3000, () => {
- console.log("Server running on port 3000");
+    console.log("Server running on port 3000");
 });
